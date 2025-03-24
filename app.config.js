88fd269040808af9d2b4c6b8a5168a15998b76fd
Gustavo Falcão <gustavo.falcao01@gmail.com>
2025-03-24 11:25:59 +0000

    Migrate from app.json to app.config.js for environment variable support

---
+++ app.config.js
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "OmniSocial",
+    slug: "OmniSocial",
+    version: "0.0.12",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    newArchEnabled: true,
+    // Environment variables for Firebase config
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
+  },
+};
